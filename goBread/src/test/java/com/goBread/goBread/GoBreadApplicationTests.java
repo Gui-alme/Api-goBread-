b23066964f1bf1631ba418e7af3b56965853c094
Gui-alme <guilherme.almeida@sptech.school>
2023-10-16 09:59:47 -0300

    setup inicial

---
+++ goBread/src/test/java/com/goBread/goBread/GoBreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goBread.goBread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoBreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
